159af55bab6838871a90da4dbfc15687a32a7bbe
sakib007 <sakibreza1@gmail.com>
2018-01-18 00:16:22 +0600

    training & testing done

--- regression.py
+++ regression.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import quandl
+import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get("WIKI/GOOGL")
 
@@ -10,4 +14,27 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Close'] * 100
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-print(df.head())
+forcast_col = 'Adj. Close'
+df.fillna(value = -99999, inplace = True)
+forcast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forcast_col].shift(-forcast_out)
+
+df.dropna(inplace = True)
+
+X = np.array(df.drop(['label'],1))
+Y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+Y = np.array(df['label'])
+
+X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size = 0.2)
+
+clf = LinearRegression(n_jobs = -1)
+
+clf.fit(X_train,Y_train)
+
+accuracy = clf.score(X_test, Y_test)
+
+print(accuracy)
+
+#print(df.head())
